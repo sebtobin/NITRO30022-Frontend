e7bd6d7d55a1c5ac0e5db5edb8ca45590703eb35
Pepu0309 <jasontan000jt@gmail.com>
2022-09-15 23:59:59 +1000

    Added some more jest setup files and dependencies.

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,7 @@
-module.exports = api => {
-    const isTest = api.env('test');
-    // You can use isTest to determine what presets and plugins to use.
-
-    return {
-        presets: [
-            ['@babel/preset-env', {targets: {node: 'current'}}],
-            '@babel/preset-typescript',
-        ],
-    };
+module.exports = {
+    presets: [
+        ['@babel/preset-env', {targets: {node: 'current'}}],
+        '@babel/preset-typescript',
+        'next/babel'
+    ],
 };
