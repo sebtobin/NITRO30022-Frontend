342185be1e2c583de0a8b97abd348e197ed6d699
Pepu0309 <jasontan000jt@gmail.com>
2022-09-28 18:30:55 +1000

    Added some Cypress tests for logging in

--- cypress/e2e/Nitrus-Tests/nitrus.cy.js
+++ cypress/e2e/Nitrus-Tests/nitrus.cy.js
@@ -1,3 +1,5 @@
+
+
 describe('example to-do app', () => {
     beforeEach(() => {
       // At the start of all our tests, visit the home page
@@ -22,7 +24,41 @@ describe('example to-do app', () => {
           .should('have.value', '');
     })
 
-    it('Should go to the users dashboard when the user logs in', () => {
+    it('Should allow a registered user to login, and go to their dashboard', () => {
+        cy.request('POST', 'http://ec2-3-104-104-155.ap-southeast-2.compute.amazonaws.com:8081/api/users/login/', {
+            "username": "test",
+            "password": "test123"
+        })
+          .then((response) => {
+            expect(response).property('status').to.equal(200)
+            expect(response.body).property('username').to.be.a('string')
+            expect(response.body).property('token').to.be.a('string')
+          })
+        
+          cy.get('.sc-kDDrLX > form > :nth-child(1) > .sc-bczRLJ > #username').type('test')
+          cy.get(':nth-child(2) > .sc-bczRLJ > #password').type('test123')
+          cy.get('.sc-kDDrLX > form > .sc-gKXOVf').click()
+          cy.url().should('include', '/dashboard')
+          cy.contains('Welcome back to Nitrus')
+          cy.contains('My Collection')
+          cy.contains('Friends')
+    })
+
+    it('Should not allow an unregistered user to login', () => {
+      cy.request({
+        method: 'POST', 
+        url: 'http://ec2-3-104-104-155.ap-southeast-2.compute.amazonaws.com:8081/api/users/login/', 
+        body: {
+          "username": "doesnotexist",
+          "password": "doesnotexist123"
+        }, 
+        failOnStatusCode: false})
+
+        .then((response) => {
+          expect(response).property('status').to.equal(401)
+        })
+
+          cy.url().should('eq', 'http://localhost:3000/')
         
     })
 })
