97d5dbf8f804ed477c3ffff4ff81b7f2f9585d99
Pepu0309 <jasontan000jt@gmail.com>
2022-10-02 04:01:36 +1100

    Removed all explicit HTTP requests through cypress. Added some comments to the tests.

--- cypress/e2e/Nitrus-Tests/nitrus.cy.js
+++ cypress/e2e/Nitrus-Tests/nitrus.cy.js
@@ -23,68 +23,39 @@ describe('example to-do app', () => {
     })
 
     it('Should not allow a registered user to register again', () => {
-      cy.request({
-        method: 'POST', 
-        url: 'http://localhost:8000/api/users/signup/', 
-        body: {
-          "username": "test",
-          "email": "test123@gmail.com",
-          "password": "test123"
-        }, 
-        failOnStatusCode: false})
+      // Register with username "test", email "test123@gmail.com" and password "test123".
+      cy.get('.sc-iqcoie > form > :nth-child(1) > .sc-bczRLJ > #username').type('test')
+      cy.get('#email').type('test123@gmail.com')
+      cy.get(':nth-child(3) > .sc-bczRLJ > #password').type('test123')
+      cy.get('.sc-iqcoie > form > .sc-gKXOVf').click()
 
-        .then((response) => {
-          expect(response).property('status').to.equal(401)
-        })
-
-        cy.get('.sc-iqcoie > form > :nth-child(1) > .sc-bczRLJ > #username').type('test')
-        cy.get('#email').type('test123@gmail.com')
-        cy.get(':nth-child(3) > .sc-bczRLJ > #password').type('test123')
-        cy.get('.sc-iqcoie > form > .sc-gKXOVf').click()
-        cy.contains('An unexpected error occurred. Please try again.')
-        cy.url().should('eq', 'http://localhost:3000/')
+      // The registration should fail.
+      cy.contains('An unexpected error occurred. Please try again.')
+      cy.url().should('eq', 'http://localhost:3000/')
     })
 
     it('Should not allow an unregistered user to login', () => {
-      // cy.request({
-      //   method: 'POST', 
-      //   url: 'http://localhost:8000/api/users/login/', 
-      //   body: {
-      //     "username": "doesnotexist",
-      //     "password": "doesnotexist123"
-      //   }, 
-      //   failOnStatusCode: false})
+      // Attempt to login with username "doesnotexist" and password "doesnotexist123"
+      cy.get('.sc-kDDrLX > form > :nth-child(1) > .sc-bczRLJ > #username').type('doesnotexist')
+      cy.get(':nth-child(2) > .sc-bczRLJ > #password').type('doesnotexist123')
+      cy.get('.sc-kDDrLX > form > .sc-gKXOVf').click()
 
-      //   .then((response) => {
-      //     expect(response).property('status').to.equal(401)
-      //   })
-
-        cy.get('.sc-kDDrLX > form > :nth-child(1) > .sc-bczRLJ > #username').type('doesnotexist')
-        cy.get(':nth-child(2) > .sc-bczRLJ > #password').type('doesnotexist123')
-        cy.get('.sc-kDDrLX > form > .sc-gKXOVf').click()
-        cy.contains('Incorrect username or password. Please try again.')
-        cy.url().should('eq', 'http://localhost:3000/')
-        
+      // The login should fail.
+      cy.contains('Incorrect username or password. Please try again.')
+      cy.url().should('eq', 'http://localhost:3000/')
     })
 
     it('Should allow a registered user to login, and go to their dashboard', () => {
-        // cy.request('POST', 'http://localhost:8000/api/users/login/', {
-        //     "username": "test",
-        //     "password": "test123"
-        // })
-        //   .then((response) => {
-        //     expect(response).property('status').to.equal(200)
-        //     expect(response.body).property('username').to.be.a('string')
-        //     expect(response.body).property('token').to.be.a('string')
-        //   })
-        
-        cy.get('.sc-kDDrLX > form > :nth-child(1) > .sc-bczRLJ > #username').type('test')
-        cy.get(':nth-child(2) > .sc-bczRLJ > #password').type('test123')
-        cy.get('.sc-kDDrLX > form > .sc-gKXOVf').click()
-        cy.url().should('include', '/dashboard')
-        cy.contains('Welcome back to Nitrus')
-        cy.contains('My Collection')
-        cy.contains('Friends')
+      // Attempt to login with username "test" and password "test123"
+      cy.get('.sc-kDDrLX > form > :nth-child(1) > .sc-bczRLJ > #username').type('test')
+      cy.get(':nth-child(2) > .sc-bczRLJ > #password').type('test123')
+      cy.get('.sc-kDDrLX > form > .sc-gKXOVf').click()
+
+      // The login should be successful and Nitrus should navigate to the dashboard
+      cy.url().should('include', '/dashboard')
+      cy.contains('Welcome back to Nitrus')
+      cy.contains('My Collection')
+      cy.contains('Friends')
     })
 
 })
