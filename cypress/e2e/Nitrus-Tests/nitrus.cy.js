3dadf44b1a67eef340e6a89e369c85986b34a891
Pepu0309 <jasontan000jt@gmail.com>
2022-10-12 18:50:58 +1100

    Changed back the cy.get lines.

--- cypress/e2e/Nitrus-Tests/nitrus.cy.js
+++ cypress/e2e/Nitrus-Tests/nitrus.cy.js
@@ -13,12 +13,12 @@ describe('example to-do app', () => {
     it('Fields should contain input when typed in and clear properly', () => {
         cy.contains('Login')
         
-        cy.get('.sc-ivTmOn > form > :nth-child(1) > .sc-hAZoDl > #username').type('isaac-parsons')
+        cy.get('.sc-kDDrLX > form > :nth-child(1) > .sc-bczRLJ > #username').type('isaac-parsons')
           .should('have.value', 'isaac-parsons')
           .clear()
           .should('have.value', '');
 
-        cy.get(':nth-child(2) > .sc-hAZoDl > #password').type('gnocci123')
+        cy.get(':nth-child(2) > .sc-bczRLJ > #password').type('gnocci123')
           .should('have.value', 'gnocci123')
           .clear()
           .should('have.value', '');
