0594a37fc7678ff7d77ffbac80de928ab1a6ae93
Pepu0309 <jasontan000jt@gmail.com>
2022-10-20 00:48:17 +1100

    Added back the profile page text fields. Switch the URL to the public backend temporarily for testing.

--- src/redux/apiClient.ts
+++ src/redux/apiClient.ts
@@ -9,8 +9,8 @@ import {
   PostCollection,
 } from "./apiTypes";
 import { RootState } from "./store";
-// const baseUrl = `http://ec2-3-104-104-155.ap-southeast-2.compute.amazonaws.com:8081/api`;
-const baseUrl = `http://0.0.0.0:8081/api`;
+const baseUrl = `http://ec2-3-104-104-155.ap-southeast-2.compute.amazonaws.com:8081/api`;
+// const baseUrl = `http://0.0.0.0:8081/api`;
 export const nitrusApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: baseUrl,
