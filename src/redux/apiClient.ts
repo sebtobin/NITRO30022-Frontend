ddf5aa42cf4b037a1bf857e383964bf2c5a946c4
surya-ven <84949099+surya-ven@users.noreply.github.com>
2022-10-19 21:22:36 +1100

    Added eslintignore for debugging purpose

--- src/redux/apiClient.ts
+++ src/redux/apiClient.ts
@@ -9,8 +9,8 @@ import {
   PostCollection,
 } from "./apiTypes";
 import { RootState } from "./store";
-export const baseUrl = `http://ec2-3-104-104-155.ap-southeast-2.compute.amazonaws.com:8081/api`;
-// export const baseUrl = `http://0.0.0.0:8081/api`;
+// export const baseUrl = `http://ec2-3-104-104-155.ap-southeast-2.compute.amazonaws.com:8081/api`;
+export const baseUrl = `http://localhost:8081/api`;
 export const nitrusApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: baseUrl,
