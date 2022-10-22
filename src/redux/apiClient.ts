6eb62910280ed61fa1546b631e3362a1461ea003
surya-ven <84949099+surya-ven@users.noreply.github.com>
2022-10-22 20:06:10 +1100

    Changed to new url 2

--- src/redux/apiClient.ts
+++ src/redux/apiClient.ts
@@ -10,7 +10,7 @@ import {
 } from "./apiTypes";
 import { RootState } from "./store";
 // export const baseUrl = `http://localhost:8081/api`;
-export const baseUrl = `http://ec2-3-26-229-20.ap-southeast-2.compute.amazonaws.com/api`;
+export const baseUrl = `http://ec2-3-26-229-20.ap-southeast-2.compute.amazonaws.com:8081/api`;
 export const nitrusApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: baseUrl,
