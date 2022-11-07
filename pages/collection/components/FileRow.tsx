f501ea3e417b4f67117f6f7ae93553654e8c9fdb
surya-ven <84949099+surya-ven@users.noreply.github.com>
2022-11-07 21:47:41 +1100

    include right function for changing string, and change file url to be correct

--- pages/collection/components/FileRow.tsx
+++ pages/collection/components/FileRow.tsx
@@ -13,26 +13,44 @@ interface FileRowProps {
   file: FileData;
 }
 
+function setCharAt(str:any, index:any, chr:any) {
+	if (index > str.length - 1) return str;
+	return str.substring(0, index) + chr + str.substring(index + 1);
+}
+
 const FileRow: FC<FileRowProps> = ({ onView, onDelete, onEdit, file }) => {
   return (
-    <FilePill>
-      <FileName>
-        {file.title.length > 17
-          ? file.title.substring(0, 17) + "..."
-          : file.title}
-      </FileName>
-      <FileButtons>
-        <FileButton href={file.document} target="_blank">
-          <Image src={ViewButton} alt={""} />
-        </FileButton>
-        <FileButton onClick={onEdit}>
-          <Image src={EditButton} alt={""} />
-        </FileButton>
-        <FileButton>
-          <Image onClick={onDelete} src={DeleteButton} alt={""} />
-        </FileButton>
-      </FileButtons>
-    </FilePill>
+		<FilePill>
+			<FileName>
+				{file.title.length > 17
+					? file.title.substring(0, 17) + "..."
+					: file.title}
+			</FileName>
+			<FileButtons>
+				<FileButton
+					href={
+						file.document.indexOf(".ap-southeast-3") > 0
+							? setCharAt(
+									file.document,
+									file.document.indexOf(".ap-southeast-3") +
+										".ap-southeast-3".length -
+										1,
+									"2"
+							  )
+							: file.document
+					}
+					target="_blank"
+				>
+					<Image src={ViewButton} alt={""} />
+				</FileButton>
+				<FileButton onClick={onEdit}>
+					<Image src={EditButton} alt={""} />
+				</FileButton>
+				<FileButton>
+					<Image onClick={onDelete} src={DeleteButton} alt={""} />
+				</FileButton>
+			</FileButtons>
+		</FilePill>
   );
 };
 
